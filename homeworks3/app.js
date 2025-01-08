02185af424cc128541fb13d2596699fbecba4c92
Jessy <yasminbits@gmail.com>
2025-01-08 16:10:19 +0200

    Fixed corrupted file

--- homeworks3/app.js
+++ homeworks3/app.js
@@ -1,2 +1,12 @@
 const http = require ('http');
 const fs = require ('fs');
+const path = require('path');
+const dirPath= path.join(__dirname , '/templates');
+
+const file2send= fs.readFileSync(`templates/page.html`)
+
+const server = http.createServer(function(req , res){
+  res.end(file2send);
+});
+server.listen(3000);
+console.log('node.js web server at port 3000 is running');
